bf279255b7718a95282910a85148062363821470
Jasper Teo <43600109+jasperteo2412@users.noreply.github.com>
2026-03-04 16:50:10 +0800

    Update build.sh

--- llm-frontend-python/build.sh
+++ llm-frontend-python/build.sh
@@ -1,2 +1,2 @@
-  docker build --platform linux/arm64 -t jttxncs/llm-frontend-python:latest .        
-  docker push jttxncs/llm-frontend-python:latest 
+  docker build --platform linux/arm64 -t jttxncs/llm-frontend-python:scan .        
+  docker push jttxncs/llm-frontend-python:scan 
--- llm-multiroute/build.sh
+++ llm-multiroute/build.sh
@@ -1,2 +1,2 @@
-  docker build --platform linux/arm64 -t jttxncs/llm-multiroute:latest .        
-  docker push jttxncs/llm-multiroute:latest 
+  docker build --platform linux/arm64 -t jttxncs/llm-multiroute:scan .        
+  docker push jttxncs/llm-multiroute:scan 
